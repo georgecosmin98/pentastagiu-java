5b643305579275e0cdd612b30de42252f58a4e49
GeorgeCosminIlinca <ilincageorgecosmin@yahoo.com>
2018-12-17 09:30:48 +0200

    .

--- Course3HomeWork/src/Shop/Basket.java
+++ Course3HomeWork/src/Shop/Basket.java
@@ -9,6 +9,11 @@ public class Basket {
         this.customer = customer;
     }
 
+    public Basket(Customer customer) {
+        this.customer = customer;
+        totalPrice = 0;
+    }
+
     public double getTotalPrice() {
         return totalPrice;
     }
@@ -25,8 +30,39 @@ public class Basket {
         this.customer = customer;
     }
 
-    public void addItemInBasket(double priceOfProduct,int quantity) {
-        totalPrice += priceOfProduct;
+    public void addBookInBasket(Book book) {
+        totalPrice += book.getPrice();
+        System.out.println("Total: " + totalPrice);
+    }
+
+    public void addCandyInBasket(Candy candy, int quantity) {
+        if (quantity <= candy.getQuantity()) {
+            totalPrice += candy.getPrice() * quantity;
+            candy.setQuantity(candy.getQuantity() - quantity);
+            System.out.println("Total: " + totalPrice);
+        } else
+            System.out.println("Sorry! We have just " + candy.getQuantity() + " pcs!");
+    }
+
+    public void cashOut() {
+        if (customer.getMembership() == Customer.membership.Gold) {
+            System.out.println("The price you have to pay is: " + totalPrice * 0.8);
+            System.out.println("You saved: " + totalPrice * 0.2);
+        } else if (customer.getMembership() == Customer.membership.Silver) {
+            System.out.println("The price you have to pay is: " + totalPrice * 0.9);
+            System.out.println("You saved: " + totalPrice * 0.10);
+        } else
+            System.out.println("The price you have to pay is: " + totalPrice);
+        totalPrice = 0;
+    }
+
+    public void totalPricePrinter() {
+        if (customer.getMembership() == Customer.membership.Gold) {
+            System.out.println("The price you have to pay is: " + totalPrice * 0.8);
+        } else if (customer.getMembership() == Customer.membership.Silver) {
+            System.out.println("The price you have to pay is: " + totalPrice * 0.9);
+        } else
+            System.out.println("The price you have to pay is: " + totalPrice);
 
     }
 }
--- Course3HomeWork/src/Shop/Shop.java
+++ Course3HomeWork/src/Shop/Shop.java
@@ -2,18 +2,32 @@ package Shop;
 
 public class Shop {
     public static void main(String[] args) {
+        //Create a customer
         Customer firstCustomer = new Customer(1, "Ionut", Customer.membership.Gold);
-        Customer secondCustomer = new Customer(2, "Mariana", Customer.membership.Nomembership);
-        Customer thirdPerson = new Customer(3, "Bianca", Customer.membership.Silver);
 
-        Book firstBook=new Book(1,"Sun Tzu: The Art of Warfare",100,"Roger T. Ames");
-        Book secondBook=new Book(2,"The Hunger Games",52,"Suzanne Collins");
-        Book thirdBook=new Book(3,"The Lord of the Rings",74,"J.R.R. Tolkien");
+        //Create products
+        Book firstBook = new Book(1, "Sun Tzu: The Art of Warfare", 100, "Roger T. Ames");
+        Book secondBook = new Book(2, "The Hunger Games", 52, "Suzanne Collins");
+        Book thirdBook = new Book(3, "The Lord of the Rings", 74, "J.R.R. Tolkien");
 
-        Candy firstCandy=new Candy(1,"Toblerone",12,120);
-        Candy secondCandy=new Candy(2,"Mars",5,80);
-        Candy thirdCandy=new Candy(3,"Raffaello",16,70);
+        Candy firstCandy = new Candy(1, "Toblerone", 12, 120);
+        Candy secondCandy = new Candy(2, "Mars", 5, 80);
+        Candy thirdCandy = new Candy(3, "Raffaello", 16, 70);
 
+        //Create basket
+        Basket firstBasket = new Basket(firstCustomer);
 
+        //Add products to the basket
+        firstBasket.addBookInBasket(firstBook);
+        firstBasket.addCandyInBasket(secondCandy, 50);
+
+        //Get info about the discounted price
+        firstBasket.totalPricePrinter();
+
+        //Chenge the membership
+        firstCustomer.setMembership(Customer.membership.Silver);
+
+        //Print new discounted price
+        firstBasket.cashOut();
     }
 }
